13d395c9d5e26e40580a1e54f8b20c811631bfeb
Admiral <oralhansteves60@gmail.com>
2026-02-03 17:59:55 +0500

    3

--- practice 2/python if else/else.py
+++ practice 2/python if else/else.py
@@ -1,6 +1,6 @@
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
+number = 7
+
+if number % 2 == 0:
+  print("The number is even")
 else:
-  print("b is not greater than a")
+  print("The number is odd")
