1f4fc113565c8aa8aff3e74da86bcbb2b31a77cd
Admiral <oralhansteves60@gmail.com>
2026-02-16 19:39:28 +0500

    7

--- practice 3/functions/args,kwargs.py
+++ practice 3/functions/args,kwargs.py
@@ -1,4 +1,7 @@
-def my_function(**kid):
-  print("His last name is " + kid["lname"])
+def my_function(**myvar):
+  print("Type:", type(myvar))
+  print("Name:", myvar["name"])
+  print("Age:", myvar["age"])
+  print("All data:", myvar)
 
-my_function(fname = "Tobias", lname = "Refsnes")
+my_function(name = "Tobias", age = 30, city = "Bergen")
