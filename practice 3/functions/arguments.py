60991f63b9d943581c7f77446d930dacfa0c3254
Admiral <oralhansteves60@gmail.com>
2026-02-16 18:47:52 +0500

    20

--- practice 3/functions/arguments.py
+++ practice 3/functions/arguments.py
@@ -1,4 +1,5 @@
-def my_function(name):
-  print("Hello", name)
+def my_function(a, b, /, *, c, d):
+  return a + b + c + d
 
-my_function("Emil")
+result = my_function(5, 10, c = 15, d = 20)
+print(result)
