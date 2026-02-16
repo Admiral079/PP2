4f30691a9dbcd187f259b6f51040f3a37e2e97ca
Admiral <oralhansteves60@gmail.com>
2026-02-16 18:38:13 +0500

    7

--- practice 3/functions/arguments.py
+++ practice 3/functions/arguments.py
@@ -2,4 +2,4 @@ def my_function(animal, name):
   print("I have a", animal)
   print("My", animal + "'s name is", name)
 
-my_function(animal = "dog", name = "Buddy")
+my_function(name = "Buddy", animal = "dog")
