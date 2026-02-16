d673f2fcc88857a181d9fe66f4c86a29357bf751
Admiral <oralhansteves60@gmail.com>
2026-02-16 18:41:40 +0500

    14

--- practice 3/functions/arguments.py
+++ practice 3/functions/arguments.py
@@ -1,5 +1,7 @@
-def my_function(x, y):
-  return x + y
+def my_function():
+  return ["apple", "banana", "cherry"]
 
-result = my_function(5, 3)
-print(result)
+fruits = my_function()
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
