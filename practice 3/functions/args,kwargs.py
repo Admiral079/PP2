1705c78de0dcaad76bc2772271bb1afde7311fdc
Admiral <oralhansteves60@gmail.com>
2026-02-16 19:32:53 +0500

    2

--- practice 3/functions/args,kwargs.py
+++ practice 3/functions/args,kwargs.py
@@ -1,4 +1,7 @@
-def my_function(*kids):
-  print("The youngest child is " + kids[2])
+def my_function(*args):
+  print("Type:", type(args))
+  print("First argument:", args[0])
+  print("Second argument:", args[1])
+  print("All arguments:", args)
 
 my_function("Emil", "Tobias", "Linus")
