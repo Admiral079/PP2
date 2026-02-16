33f7119b71e76ce98c4e06394654ded5f1f107f2
Admiral <oralhansteves60@gmail.com>
2026-02-16 19:39:03 +0500

    6

--- practice 3/functions/args,kwargs.py
+++ practice 3/functions/args,kwargs.py
@@ -1,10 +1,4 @@
-def my_function(*numbers):
-  if len(numbers) == 0:
-    return None
-  max_num = numbers[0]
-  for num in numbers:
-    if num > max_num:
-      max_num = num
-  return max_num
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
 
-print(my_function(3, 7, 2, 9, 1))
+my_function(fname = "Tobias", lname = "Refsnes")
