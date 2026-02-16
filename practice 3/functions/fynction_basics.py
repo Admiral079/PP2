3642c161ff5349d74651f7ab620097485d439e5e
Admiral <oralhansteves60@gmail.com>
2026-02-16 17:44:56 +0500

    3

--- practice 3/functions/fynction_basics.py
+++ practice 3/functions/fynction_basics.py
@@ -1,13 +1,8 @@
-temp1 = 77
-celsius1 = (temp1 - 32) * 5 / 9
-print(celsius1)
+def fahrenheit_to_celsius(fahrenheit):
+  return (fahrenheit - 32) * 5 / 9
 
-temp2 = 95
-celsius2 = (temp2 - 32) * 5 / 9
-print(celsius2)
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
 
-temp3 = 50
-celsius3 = (temp3 - 32) * 5 / 9
-print(celsius3)
-
-# without function - repetitive cods
+#reusable code with function
