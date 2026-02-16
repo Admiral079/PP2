f697a32ce62401088847a2d3c265398dd65a9608
Admiral <oralhansteves60@gmail.com>
2026-02-16 17:43:43 +0500

    2

--- practice 3/functions/fynction_basics.py
+++ practice 3/functions/fynction_basics.py
@@ -1,3 +1,13 @@
-def my_function():
-  print("Hello from a function")
-my_function()
+temp1 = 77
+celsius1 = (temp1 - 32) * 5 / 9
+print(celsius1)
+
+temp2 = 95
+celsius2 = (temp2 - 32) * 5 / 9
+print(celsius2)
+
+temp3 = 50
+celsius3 = (temp3 - 32) * 5 / 9
+print(celsius3)
+
+# without function - repetitive cods
