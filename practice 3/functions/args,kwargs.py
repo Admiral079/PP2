a266fc0d673d67d1fac6589faf45236b89ad200d
Admiral <oralhansteves60@gmail.com>
2026-02-16 19:33:25 +0500

    3

--- practice 3/functions/args,kwargs.py
+++ practice 3/functions/args,kwargs.py
@@ -1,7 +1,5 @@
-def my_function(*args):
-  print("Type:", type(args))
-  print("First argument:", args[0])
-  print("Second argument:", args[1])
-  print("All arguments:", args)
+def my_function(greeting, *names):
+  for name in names:
+    print(greeting, name)
 
-my_function("Emil", "Tobias", "Linus")
+my_function("Hello", "Emil", "Tobias", "Linus")
