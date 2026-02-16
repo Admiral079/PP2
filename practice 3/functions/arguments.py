28cb482b77b73c36889efaf6ad24b566e03debad
Admiral <oralhansteves60@gmail.com>
2026-02-16 18:11:33 +0500

    3

--- practice 3/functions/arguments.py
+++ practice 3/functions/arguments.py
@@ -1,4 +1,4 @@
-def my_function(name): # name is a parameter
-  print("Hello", name)
+def my_function(fname, lname):
+  print(fname + " " + lname)
 
-my_function("Emil") # "Emil" is an argument
+my_function("Emil", "Refsnes")
