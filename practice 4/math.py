ed8f57b789aa2e8fdc85d3e8e4a269525f3a9cc7
Admiral <oralhansteves60@gmail.com>
2026-02-17 23:46:44 +0500

    1

--- practice 4/math.py
+++ practice 4/math.py
@@ -1,4 +1,19 @@
-numbers = [4, 2, 9, 1]
+import math
 
-print(min(numbers))
-print(max(numbers))
+print(abs(-7))
+print(pow(2, 3))
+print(math.sqrt(25))
+
+#okruglenie
+print(round(3.6))
+print(round(3.14159, 2))
+print(math.ceil(3.2))
+print(math.floor(3.8))
+
+#trigo
+print(math.sin(math.pi / 2))
+print(math.cos(0))
+
+#constants
+print(math.pi)
+print(math.e)
