28c9ff2b46025be715d17905ed57494371e0f84a
Admiral <oralhansteves60@gmail.com>
2026-02-17 22:53:10 +0500

    3

--- practice 4/generaters.py
+++ practice 4/generaters.py
@@ -1,11 +1,7 @@
-def my_generator():
-    yield 1
-    yield 2
-    yield 3
+def count_up_to(n):
+    for i in range(1, n + 1):
+        yield i
 
 
-gen = my_generator()
-
-print(next(gen))
-print(next(gen))
-print(next(gen))
+for num in count_up_to(5):
+    print(num)
