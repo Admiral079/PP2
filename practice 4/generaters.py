e78d528fe69e04f9e516a6bd942b9580bd862db8
Admiral <oralhansteves60@gmail.com>
2026-02-17 22:58:31 +0500

    4

--- practice 4/generaters.py
+++ practice 4/generaters.py
@@ -1,7 +1,4 @@
-def count_up_to(n):
-    for i in range(1, n + 1):
-        yield i
+gen = (x*x for x in range(5))
 
-
-for num in count_up_to(5):
-    print(num)
+for value in gen:
+    print(value)
