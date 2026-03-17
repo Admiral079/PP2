3e34ee99398b9fc75a4878651f1d1f7f85d85a27
Admiral <oralhansteves60@gmail.com>
2026-03-17 22:57:13 +0500

    4

---
+++ practice 6/Directory_management/create_list_dirs.py
@@ -0,0 +1,19 @@
+#1
+import os
+os.mkdir("dir1")
+
+#2
+import os
+os.makedirs("a/b/c")
+
+#3
+import os
+print(os.listdir("."))
+
+#4
+import os
+print(os.getcwd())
+
+#5
+import os
+os.rmdir("dir1")
